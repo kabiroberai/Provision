a460cd581f38143591a051e63631d9f9c6ddf3cb
Kabir Oberai <oberai.kabir@gmail.com>
2024-11-22 00:17:36 -0800

    Update types

--- provision_c/exports.d
+++ provision_c/exports.d
@@ -10,42 +10,42 @@ public extern(C) void provision_Load(const char* path) {
     gADI = new ADI(cast(string) path.fromStringz);
 }
 
-public extern(C) void provision_SetAndroidID(const char* identifier, uint length) {
-    androidInvoke!gADI.pADISetAndroidID(identifier, length);
+public extern(C) int provision_SetAndroidID(const char* identifier, uint length) {
+    return androidInvoke!gADI.pADISetAndroidID(identifier, length);
 }
 
-public extern(C) void provision_SetProvisioningPath(const char* path) {
-    androidInvoke!gADI.pADISetProvisioningPath(path);
+public extern(C) int provision_SetProvisioningPath(const char* path) {
+    return androidInvoke!gADI.pADISetProvisioningPath(path);
 }
 
-public extern(C) void provision_ProvisioningErase(ulong dsId) {
-    androidInvoke!gADI.pADIProvisioningErase(dsId);
+public extern(C) int provision_ProvisioningErase(ulong dsId) {
+    return androidInvoke!gADI.pADIProvisioningErase(dsId);
 }
 
-public extern(C) void provision_Synchronize(ulong dsId, ubyte* serverIntermediateMetadata, uint serverIntermediateMetadataLength, ubyte** machineIdentifier, uint* machineIdentifierLength, ubyte** synchronizationResumeMetadata, uint* synchronizationResumeMetadataLength) {
-    androidInvoke!gADI.pADISynchronize(dsId, serverIntermediateMetadata, serverIntermediateMetadataLength, machineIdentifier, machineIdentifierLength, synchronizationResumeMetadata, synchronizationResumeMetadataLength);
+public extern(C) int provision_Synchronize(ulong dsId, ubyte* serverIntermediateMetadata, uint serverIntermediateMetadataLength, ubyte** machineIdentifier, uint* machineIdentifierLength, ubyte** synchronizationResumeMetadata, uint* synchronizationResumeMetadataLength) {
+    return androidInvoke!gADI.pADISynchronize(dsId, serverIntermediateMetadata, serverIntermediateMetadataLength, machineIdentifier, machineIdentifierLength, synchronizationResumeMetadata, synchronizationResumeMetadataLength);
 }
 
-public extern(C) void provision_ProvisioningDestroy(uint session) {
-    androidInvoke!gADI.pADIProvisioningDestroy(session);
+public extern(C) int provision_ProvisioningDestroy(uint session) {
+    return androidInvoke!gADI.pADIProvisioningDestroy(session);
 }
 
-public extern(C) void provision_ProvisioningEnd(uint session, ubyte* persistentTokenMetadata, uint persistentTokenMetadataLength, ubyte* trustKey, uint trustKeyLength) {
-    androidInvoke!gADI.pADIProvisioningEnd(session, persistentTokenMetadata, persistentTokenMetadataLength, trustKey, trustKeyLength);
+public extern(C) int provision_ProvisioningEnd(uint session, ubyte* persistentTokenMetadata, uint persistentTokenMetadataLength, ubyte* trustKey, uint trustKeyLength) {
+    return androidInvoke!gADI.pADIProvisioningEnd(session, persistentTokenMetadata, persistentTokenMetadataLength, trustKey, trustKeyLength);
 }
 
-public extern(C) void provision_ProvisioningStart(ulong dsId, ubyte* serverProvisioningIntermediateMetadata, uint serverProvisioningIntermediateMetadataLength, ubyte** clientProvisioningIntermediateMetadata, uint* clientProvisioningIntermediateMetadataLength, uint* session) {
-    androidInvoke!gADI.pADIProvisioningStart(dsId, serverProvisioningIntermediateMetadata, serverProvisioningIntermediateMetadataLength, clientProvisioningIntermediateMetadata, clientProvisioningIntermediateMetadataLength, session);
+public extern(C) int provision_ProvisioningStart(ulong dsId, ubyte* serverProvisioningIntermediateMetadata, uint serverProvisioningIntermediateMetadataLength, ubyte** clientProvisioningIntermediateMetadata, uint* clientProvisioningIntermediateMetadataLength, uint* session) {
+    return androidInvoke!gADI.pADIProvisioningStart(dsId, serverProvisioningIntermediateMetadata, serverProvisioningIntermediateMetadataLength, clientProvisioningIntermediateMetadata, clientProvisioningIntermediateMetadataLength, session);
 }
 
 public extern(C) int provision_GetLoginCode(ulong dsId) {
     return androidInvoke!gADI.pADIGetLoginCode(dsId);
 }
 
-public extern(C) void provision_Dispose(void* ptr) {
-    androidInvoke!gADI.pADIDispose(ptr);
+public extern(C) int provision_Dispose(void* ptr) {
+    return androidInvoke!gADI.pADIDispose(ptr);
 }
 
-public extern(C) void provision_OTPRequest(ulong dsId, ubyte** machineIdentifier, uint* machineIdentifierLength, ubyte** oneTimePassword, uint* oneTimePasswordLength) {
-    androidInvoke!gADI.pADIOTPRequest(dsId, machineIdentifier, machineIdentifierLength, oneTimePassword, oneTimePasswordLength);
+public extern(C) int provision_OTPRequest(ulong dsId, ubyte** machineIdentifier, uint* machineIdentifierLength, ubyte** oneTimePassword, uint* oneTimePasswordLength) {
+    return androidInvoke!gADI.pADIOTPRequest(dsId, machineIdentifier, machineIdentifierLength, oneTimePassword, oneTimePasswordLength);
 }
